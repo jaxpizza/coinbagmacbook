1a06bc94f3c6c5aedb8a6375718a5b7ade6ba374
Jacob Wendling <jacobwendling@Jacobs-MacBook-Pro-5.local>
2024-07-12 11:31:17 -0500

    Fix build issues and update Uniswap widget configuration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { I18nProvider } from '@uniswap/widgets';
 import Header from './components/Header';
 import TokenSelection from './components/TokenSelection';
 import JennerPage from './components/JennerPage';
@@ -8,18 +7,16 @@ import JennerSwapPage from './components/JennerSwapPage';
 
 function App() {
   return (
-    <I18nProvider locale="en-US">
-      <Router>
-        <div className="App bg-gray-900 min-h-screen text-white">
-          <Header />
-          <Routes>
-            <Route path="/" element={<TokenSelection />} />
-            <Route path="/jenner" element={<JennerPage />} />
-            <Route path="/jenner/swap" element={<JennerSwapPage />} />
-          </Routes>
-        </div>
-      </Router>
-    </I18nProvider>
+    <Router>
+      <div className="App bg-gray-900 min-h-screen text-white">
+        <Header />
+        <Routes>
+          <Route path="/" element={<TokenSelection />} />
+          <Route path="/jenner" element={<JennerPage />} />
+          <Route path="/jenner/swap" element={<JennerSwapPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/JennerSwapPage.js
+++ src/components/JennerSwapPage.js
@@ -25,16 +25,7 @@ const JennerSwapPage = () => {
         <SwapWidget
           theme={theme}
           width="100%"
-          tokenList={[
-            {
-              address: JENNER_TOKEN_ADDRESS,
-              chainId: 1, // Assuming Ethereum mainnet
-              name: 'Jenner',
-              symbol: 'JENNER',
-              decimals: 18, // Assuming standard ERC20 decimals
-              logoURI: 'https://example.com/jenner-logo.png' // Replace with actual logo URL
-            }
-          ]}
+          tokenList={[JENNER_TOKEN_ADDRESS]}
           defaultOutputTokenAddress={JENNER_TOKEN_ADDRESS}
         />
       </div>
